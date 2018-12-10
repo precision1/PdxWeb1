923b40e69910f16d1c30c17575b344ba56724703
precision1 <45637000+precision1@users.noreply.github.com>
2018-12-10 13:38:03 -0800

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -63,3 +63,58 @@ function topFunction() {
 }
 
 /////////////////////////////////////////////////End Scroll Button/////////////////////////////////
+
+//Header
+// Get the modal
+let modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+let storebtn = document.getElementById("storeBtn");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+storebtn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+// Get the modal
+let homemodal = document.getElementById('homeModal');
+
+// Get the button that opens the modal
+let homebtn = document.getElementById("homeBtn");
+
+// Get the <span> element that closes the modal
+let homespan = document.getElementsByClassName("homeclose")[0];
+
+// When the user clicks the button, open the modal
+homebtn.onclick = function() {
+  homemodal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+homespan.onclick = function() {
+  homemodal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == homemodal) {
+    homemodal.style.display = "none";
+  }
+}
+
+
