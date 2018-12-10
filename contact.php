3220f8c67ce7209a6a70b4d249166a8f0a97efe5
Kayla Goldner <Kayla.Goldner@precisiondxlab.com>
2018-12-10 11:38:51 -0800

    updated 12/10 1138

--- contact.php
+++ contact.php
@@ -7,7 +7,7 @@
 $from = 'Demo contact form <demo@domain.com>';
 
 // an email address that will receive the email with the output of the form
-$sendTo = 'Demo contact form <demo@domain.com>';
+$sendTo = 'kayla.goldner@precisiondxlab.com';
 
 // subject of the email
 $subject = 'New message from contact form';
@@ -73,3 +73,5 @@ if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQ
 else {
     echo $responseArray['message'];
 }
+
+?>
--- js/contact.js
+++ js/contact.js
@@ -1,3 +1,5 @@
+
+//////////////////////////Start Email Validator////////////////////////////////////////////////////
 $(function () {
 
     // init the validator
@@ -43,3 +45,25 @@ $(function () {
         }
     })
 });
+
+/////////////////////////////////////////End Email Validator//////////////////////////////////////////////
+
+/////////////////////////////////////////Start Button Scroll/////////////////////////////////////////////
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+/////////////////////////////////////////////////End Scroll Button/////////////////////////////////
