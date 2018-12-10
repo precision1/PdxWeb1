6dfac30b8054b540a73849fceeb10aeecf4cd2f0
precision1 <45637000+precision1@users.noreply.github.com>
2018-12-10 13:43:04 -0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,13 +3,13 @@
 let modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
-let btn = document.getElementById("myBtn");
+let storebtn = document.getElementById("storeBtn");
 
 // Get the <span> element that closes the modal
 let span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal
-btn.onclick = function() {
+storebtn.onclick = function() {
     modal.style.display = "block";
 }
 
