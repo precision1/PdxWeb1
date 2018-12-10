83516de08c73166819472d816b08ee10b2e3ece6
Kayla Goldner <Kayla.Goldner@precisiondxlab.com>
2018-12-10 13:18:49 -0800

    fixed contact images

--- js/contact.js
+++ js/contact.js
@@ -2,10 +2,6 @@
 //////////////////////////Start Email Validator////////////////////////////////////////////////////
 $(function () {
 
-    // init the validator
-    // validator files are included in the download package
-    // otherwise download from http://1000hz.github.io/bootstrap-validator
-
     $('#contact-form').validator();
 
 
