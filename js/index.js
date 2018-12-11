706096a07391fc29bcc0e1b0db6f43d91b295c91
Cindy Reyes-Cortes <cindy.reyescortes@precisiondxlab.com>
2018-12-11 13:43:21 -0800

    Added services page/fixing header

--- js/index.js
+++ js/index.js
@@ -1,27 +1,27 @@
 //Header
 // Get the modal
-let modal = document.getElementById('myModal');
+let storemodal = document.getElementById('storeModal');
 
 // Get the button that opens the modal
 let storebtn = document.getElementById("storeBtn");
 
 // Get the <span> element that closes the modal
-let span = document.getElementsByClassName("close")[0];
+let storespan = document.getElementsByClassName("storeclose")[0];
 
 // When the user clicks the button, open the modal
 storebtn.onclick = function() {
-    modal.style.display = "block";
+    storemodal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
+storespan.onclick = function() {
+    storemodal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == storemodal) {
+        storemodal.style.display = "none";
     }
 }
 
