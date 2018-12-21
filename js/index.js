aebfe507698aec0059abcade0899fddc878a99b7
Kayla Goldner <Kayla.Goldner@precisiondxlab.com>
2018-12-21 12:27:04 -0800

    store done

--- js/index.js
+++ js/index.js
@@ -1,45 +1,4 @@
-
 //////////////Modal//////////////
-
-// Get the modal
-
-let storemodal = document.getElementById('storeModal');
-
-// Get the button that opens the modal
-
-let storebtn = document.getElementById("storeBtn");
-
-// Get the <span> element that closes the modal
-
-let storespan = document.getElementsByClassName("storeclose")[0];
-
-// When the user clicks the button, open the modal
-
-storebtn.onclick = function () {
-
-    storemodal.style.display = "block";
-
-}
-
-// When the user clicks on <span> (x), close the modal
-
-storespan.onclick = function () {
-
-    storemodal.style.display = "none";
-
-}
-
-// When the user clicks anywhere outside of the modal, close it
-
-window.onclick = function (event) {
-
-    if (event.target == storemodal) {
-
-        storemodal.style.display = "none";
-
-    }
-
-}
 
 // Get the modal
 
