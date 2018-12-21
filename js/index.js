f17e187314aeb6175511de90dacc6bd2786d6085
Kayla Goldner <Kayla.Goldner@precisiondxlab.com>
2018-12-21 13:07:41 -0800

    modal and menu work now

--- js/index.js
+++ js/index.js
@@ -1,80 +1,66 @@
 //////////////Modal//////////////
-
 // Get the modal
-
-let homemodal = document.getElementById('homeModal');
+var modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
-
-let homebtn = document.getElementById("homeBtn");
+var btn = document.getElementById("myBtn1");
 
 // Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-let homespan = document.getElementsByClassName("homeclose")[0];
-
-// When the user clicks the button, open the modal
-
-homebtn.onclick = function () {
-
-    homemodal.style.display = "block";
-
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-
-homespan.onclick = function () {
-
-    homemodal.style.display = "none";
-
+span.onclick = function() {
+  modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-
-window.onclick = function (event) {
-
-    if (event.target == homemodal) {
-
-        homemodal.style.display = "none";
-
-    }
-
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
+
 
 //////////////////////////////// END Modal//////////////////////////////////
 
 ////////////////////Shrinking Navbar////////////
-jQuery(document).ready(function ($) {
+// jQuery(document).ready(function ($) {
 
-    // call resizeHeader() onload if not home page
-    if (window.location.pathname !== '/') {
-        window.onload = resizeHeader()
-    }
+//     // call resizeHeader() onload if not home page
+//     if (window.location.pathname !== '/') {
+//         window.onload = resizeHeader()
+//     }
 
-    //resize header func
-    function resizeHeader() {
-        window.addEventListener('scroll', function () {
+//     //resize header func
+//     function resizeHeader() {
+//         window.addEventListener('scroll', function () {
 
-            var distanceY = Math.max(document.documentElement.scrollTop, document.body.scrollTop)
+//             var distanceY = Math.max(document.documentElement.scrollTop, document.body.scrollTop)
 
-            nav = document.querySelector(".navcustom")
+//             nav = document.querySelector(".navcustom")
 
-            if (distanceY) {
+//             if (distanceY) {
 
-                console.log('scrolled')
+//                 console.log('scrolled')
 
-                nav.classList.add("smallerNav")
-            } else {
-                if (nav.classList.contains("smallerNav")) {
+//                 nav.classList.add("smallerNav")
+//             } else {
+//                 if (nav.classList.contains("smallerNav")) {
 
-                    console.log('back to top')
+//                     console.log('back to top')
 
-                    nav.classList.remove("smallerNav")
-                }
-            }
-        })
-    }
+//                     nav.classList.remove("smallerNav")
+//                 }
+//             }
+//         })
+//     }
 
-})
+// })
 
 //////////////////End Shrinking Navbar/////////////
 
