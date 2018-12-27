d9c581896750f3980b67ff1fa80eb4d0b57a3e93
Kayla Goldner <kaylamariegoldner1@Kaylas-MacBook-Air.local>
2018-12-26 16:02:35 -0800

    about

---
+++ js/about.js
@@ -0,0 +1,50 @@
+///////Counter///////////
+var a = 0;
+$(window).scroll(function() {
+
+  var oTop = $('#counter').offset().top - window.innerHeight;
+  if (a == 0 && $(window).scrollTop() > oTop) {
+    $.fn.jQuerySimpleCounter = function( options ) {
+        var settings = $.extend({
+            start:  0,
+            end:    100,
+            easing: 'swing',
+            duration: 400,
+            complete: ''
+        }, options );
+    
+        var thisElement = $(this);
+    
+        $({count: settings.start}).animate({count: settings.end}, {
+            duration: settings.duration,
+            easing: settings.easing,
+            step: function() {
+                var mathCount = Math.ceil(this.count);
+                thisElement.text(mathCount);
+            },
+            complete: settings.complete
+        });
+    };
+    
+    
+    $('#number1').jQuerySimpleCounter({end: 345, duration: 3000});
+    $('#number2').jQuerySimpleCounter({end: 27456,duration: 3000});
+    $('#number3').jQuerySimpleCounter({end: 2810,duration: 2000});
+    $('#number4').jQuerySimpleCounter({end: 563430,duration: 5000});
+    
+    
+    
+      /* AUTHOR LINK */
+     $('.about-me-img').hover(function(){
+            $('.authorWindowWrapper').stop().fadeIn('fast').find('p').addClass('trans');
+        }, function(){
+            $('.authorWindowWrapper').stop().fadeOut('fast').find('p').removeClass('trans');
+        });
+    
+    
+      }
+    
+    });
+
+
+
