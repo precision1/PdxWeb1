9d47fb1bbc34c1037a1efbdb80d1f57b47a7b807
Kayla Goldner <Kayla.Goldner@precisiondxlab.com>
2019-01-03 10:46:15 -0800

    php

--- pages/form-to-email.php
+++ pages/form-to-email.php
@@ -21,12 +21,12 @@ if(IsInjected($visitor_email))
     exit;
 }
 
-$email_from = 'kayla.goldner@gmail.com';//<== update the email address
+$email_from = 'kayla.goldner@precisiondxlab.com';//<== update the email address
 $email_subject = "New Form submission";
 $email_body = "You have received a new message from the user $name.\n".
     "Here is the message:\n $message".
     
-$to = "kayla.goldner@gmail.com";//<== update the email address
+$to = "kayla.goldner@precisiondxlab.com";//<== update the email address
 $headers = "From: $email_from \r\n";
 $headers .= "Reply-To: $visitor_email \r\n";
 //Send the email!
