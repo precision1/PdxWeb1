1d4a3d570fbb1de4f3ea286a0ac5e69ceaf61d3d
Kayla Goldner <Kayla.Goldner@precisiondxlab.com>
2019-01-07 14:12:32 -0800

    contact form works

--- server.js
+++ server.js
@@ -5,18 +5,80 @@ var nodemailer = require('nodemailer');
 var app = express();
 
 
+// var transporter = nodemailer.createTransport({
+//     service: 'Gmail',
+//     auth: {
+//         user: 'hrprecisiondx@gmail.com',
+//         pass: 'Pdxlab123!'
+//     }
+// });
+
+// console.log('created');
+// transporter.sendMail({
+// from: 'hrprecisiondx@gmail.com',
+//   to: 'hrprecisiondx@gmail.com',
+//   subject: '',
+//   text: ''
+// });
+
 var transporter = nodemailer.createTransport({
-    service: 'Gmail',
+    service: 'gmail',
     auth: {
-        user: 'hrprecisiondx@gmail.com',
-        pass: 'Pdxlab123!'
-    }
+           user: 'hrprecisiondx@gmail.com',
+           pass: 'Pdxlab123!'
+       }
+   });
+
+//    const mailOptions = {
+//     from: '#user_email', // sender address
+//     to: 'hrprecisiondx@gmail.com', // list of receivers
+//     subject: 'Contact Form Submission - Precision Website', // Subject line
+//     html: '#textarea1'// plain text body
+//   };
+
+//   transporter.sendMail(mailOptions, function (err, info) {
+//     if(err)
+//       console.log(err)
+//     else
+//       console.log(info);
+//  });
+
+
+
+
+app.use(express.static(__dirname + '../pages'));
+
+app.get('../', function (req, res) {
+    res.sendfile('../pages/contact.html');
 });
 
-console.log('created');
-transporter.sendMail({
-from: 'hrprecisiondx@gmail.com',
-  to: 'hrprecisiondx@gmail.com',
-  subject: 'hello world!',
-  text: 'hello world!'
+app.get('/send', function (req, res) {
+
+    var mailOptions = {
+        to: req.query.to,
+        subject: 'Contact Form Message',
+        from: "Contact Form Request" + "<" + req.query.from + '>',
+        html:  "From: " + req.query.name + "<br>" +
+               "User's email: " + req.query.user + "<br>" +     "Message: " + req.query.text
+    }
+
+    console.log(mailOptions);
+    transporter.sendMail(mailOptions, function (err, response) {
+        if (err) {
+            console.log(err);
+            res.end("error");
+        } else {
+            console.log("Message sent: " + response.message);
+            res.end("sent");
+        }
+    });
+
+});
+
+app.listen(8080, function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Listening on port on 8080");
+    }
 });
