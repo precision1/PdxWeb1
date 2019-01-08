347e84b86675c87e162c4a84d43a24e5e60319b9
Kayla Goldner <Kayla.Goldner@precisiondxlab.com>
2019-01-08 10:55:32 -0800

    .

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -16,27 +16,27 @@ module.exports = function(app) {
   // ---------------------------------------------------------------------------
 
   app.get("/index", function(req, res) {
-    res.sendFile(path.join(__dirname, "./pages/index.html"));
+    res.sendFile(path.join(__dirname, "../pages/index.html"));
   });
 
   app.get("/services", function(req, res) {
-    res.sendFile(path.join(__dirname, "./pages/services.html"));
+    res.sendFile(path.join(__dirname, "../pages/services.html"));
   });
   app.get("/privacy", function(req, res) {
-    res.sendFile(path.join(__dirname, "./pages/privacy.html"));
+    res.sendFile(path.join(__dirname, "../pages/privacy.html"));
   });
 
   app.get("/contact", function(req, res) {
-    res.sendFile(path.join(__dirname, "./pages/contact.html"));
+    res.sendFile(path.join(__dirname, "../pages/contact.html"));
   });
   app.get("/careers", function(req, res) {
-    res.sendFile(path.join(__dirname, "./pages/careers.html"));
+    res.sendFile(path.join(__dirname, "../pages/careers.html"));
   });
   app.get("/about", function(req, res) {
-    res.sendFile(path.join(__dirname, "./pages/about.html"));
+    res.sendFile(path.join(__dirname, "../pages/about.html"));
   });
   // If no matching route is found default to home
   app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./pages/index.html"));
+    res.sendFile(path.join(__dirname, "../pages/index.html"));
   });
 };
