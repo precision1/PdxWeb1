191d285645b789c0c3c06851b0e984f52dddc3f8
Kayla Goldner <Kayla.Goldner@precisiondxlab.com>
2019-01-09 13:37:10 -0800

    timer

--- js/about.js
+++ js/about.js
@@ -6,10 +6,10 @@ $(window).scroll(function() {
   if (a == 0 && $(window).scrollTop() > oTop) {
     $.fn.jQuerySimpleCounter = function( options ) {
         var settings = $.extend({
-            start:  0,
+            start:  50,
             end:    100,
             easing: 'swing',
-            duration: 400,
+            duration: 43200000,
             complete: ''
         }, options );
     
@@ -29,10 +29,10 @@ $(window).scroll(function() {
     };
     
     
-    $('#number1').jQuerySimpleCounter({end: 345, duration: 3000});
-    $('#number2').jQuerySimpleCounter({end: 27456,duration: 300});
-    $('#number3').jQuerySimpleCounter({end: 2810,duration: 800});
-    $('#number4').jQuerySimpleCounter({end: 563430,duration: 300});
+    $('#number1').jQuerySimpleCounter({end: 345, duration: 100});
+    $('#number2').jQuerySimpleCounter({end: 27456,duration: 1000000});
+    $('#number3').jQuerySimpleCounter({end: 2810,duration: 1000000});
+    $('#number4').jQuerySimpleCounter({end: 563430,duration: 43200000});
     
     
     
@@ -48,4 +48,63 @@ $(window).scroll(function() {
     
     });
 
+/*12 hour Counter*/
 
+// (function($) {
+//     $.fn.countTo = function(options) {
+//         // merge the default plugin settings with the custom options
+//         options = $.extend({}, $.fn.countTo.defaults, options || {});
+
+//         // how many times to update the value, and how much to increment the value on each update
+//         var loops = Math.ceil(options.speed / options.refreshInterval),
+//             increment = (options.to - options.from) / loops;
+
+//         return $(this).each(function() {
+//             var _this = this,
+//                 loopCount = 0,
+//                 value = options.from,
+//                 interval = setInterval(updateTimer, options.refreshInterval);
+
+//             function updateTimer() {
+//                 value += increment;
+//                 loopCount++;
+//                 $(_this).html(value.toFixed(options.decimals));
+
+//                 if (typeof(options.onUpdate) == 'function') {
+//                     options.onUpdate.call(_this, value);
+//                 }
+
+//                 if (loopCount >= loops) {
+//                     clearInterval(interval);
+//                     value = options.to;
+
+//                     if (typeof(options.onComplete) == 'function') {
+//                         options.onComplete.call(_this, value);
+//                     }
+//                 }
+//             }
+//         });
+//     };
+
+//     $.fn.countTo.defaults = {
+//         from: 0,  // the number the element should start at
+//         to: 100,  // the number the element should end at
+//         speed: 1000,  // how long it should take to count between the target numbers
+//         refreshInterval: 100,  // how often the element should be updated
+//         decimals: 0,  // the number of decimal places to show
+//         onUpdate: null,  // callback method for every time the element is updated,
+//         onComplete: null,  // callback method for when the element finishes updating
+//     };
+// })(jQuery);
+
+// jQuery(function($) {
+//         $('#number2').countTo({
+//             from: 50,
+//             to: 2500,
+//             speed: 1000000,
+//             refreshInterval: 50,
+//             onComplete: function(value) {
+//                 console.debug(this);
+//             }
+//         });
+//     });
