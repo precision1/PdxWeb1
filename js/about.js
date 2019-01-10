08e627e958d75647be7cb3297a0c4878e5d2a404
Kayla Goldner <Kayla.Goldner@precisiondxlab.com>
2019-01-10 12:10:07 -0800

    push

--- js/about.js
+++ js/about.js
@@ -1,39 +1,39 @@
 ///////Counter///////////
 
-var a = 0;
-$(window).scroll(function() {
+// var a = 0;
+// $(window).scroll(function() {
 
-  var oTop = $('#counter').offset().top - window.innerHeight;
-  if (a == 0 && $(window).scrollTop() > oTop) {
-    $.fn.jQuerySimpleCounter = function( options ) {
-        var settings = $.extend({
-            start:  0,
-            end:    100,
-            easing: 'swing',
-            duration: 43200000,
-            complete: ''
-        }, options );
+//   var oTop = $('#counter').offset().top - window.innerHeight;
+//   if (a == 0 && $(window).scrollTop() > oTop) {
+//     $.fn.jQuerySimpleCounter = function( options ) {
+//         var settings = $.extend({
+//             start:  0,
+//             end:    100,
+//             easing: 'swing',
+//             duration: 43200000,
+//             complete: ''
+//         }, options );
     
-        var thisElement = $(this);
+//         var thisElement = $(this);
     
-        $({count: settings.start}).animate({count: settings.end}, {
-            duration: settings.duration,
-            easing: settings.easing,
-            step: function() {
-                var mathCount = Math.ceil(this.count);
-                thisElement.text(mathCount);
-            },
-            complete: settings.complete
-        });
+//         $({count: settings.start}).animate({count: settings.end}, {
+//             duration: settings.duration,
+//             easing: settings.easing,
+//             step: function() {
+//                 var mathCount = Math.ceil(this.count);
+//                 thisElement.text(mathCount);
+//             },
+//             complete: settings.complete
+//         });
 
-        a=1;
-    };
+//         a=1;
+//     };
     
     
-    $('#number1').jQuerySimpleCounter({end: 345, duration: 100});
-    $('#number2').jQuerySimpleCounter({end: 27456,duration: 43200000});
-    $('#number3').jQuerySimpleCounter({end: 2810,duration: 43200000});
-    $('#number4').jQuerySimpleCounter({end: 563430,duration: 43200000});
+//     $('#number1').jQuerySimpleCounter({end: 345, duration: 100});
+//     $('#number2').jQuerySimpleCounter({end: 27456,duration: 43200000});
+//     $('#number3').jQuerySimpleCounter({end: 2810,duration: 43200000});
+//     $('#number4').jQuerySimpleCounter({end: 563430,duration: 43200000});
     
     
     
