512d7380c739a37ae4c15be2105bb75f1a2beced
Kayla Goldner <Kayla.Goldner@precisiondxlab.com>
2019-01-22 15:52:27 -0800

    fixed video on service page

--- js/services.js
+++ js/services.js
@@ -1,3 +1,13 @@
 $(".js-modal-btn").modalVideo();
 
-
+/*////// Services Video////*/
+$(function() {
+    $('.no-button, .popup-vimeo').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+        fixedContentPos: false
+    });
+});
