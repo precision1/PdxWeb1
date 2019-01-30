ad76f1591f9ac9ee39990a58e73b7c3706cd4773
Kayla Goldner <Kayla.Goldner@precisiondxlab.com>
2019-01-29 16:32:15 -0800

    services done

--- js/services.js
+++ js/services.js
@@ -118,3 +118,18 @@ $(".popup__close").click(function() {
 });
 
 
+////Popup modal for both modal youtube videos/////////////
+$('.my-btnn').on('click', function(){
+  $('#'+$(this).data('modal')).css('display','block');
+})
+
+$('.close').on('click', function(){
+  $('.modal').hide();
+})
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target.className == 'modal') {
+    $('.modal').hide();
+  }
+}
